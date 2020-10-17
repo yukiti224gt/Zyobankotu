761271ba0e72c64202358d83ce55c46aaff74fb3
yukiti224gt <mamekiti084@gmail.com>
2020-10-17 11:32:03 +0900

    画像削除

--- main.py
+++ main.py
@@ -90,64 +90,63 @@ def on_follow(event):
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # 時刻表と入力されたら画像を送信する
-    if event.message.text == "時刻表":
-        image_message = ImageSendMessage(
-            original_content_url=f"https://linebot-practice-12.herokuapp.com/static/jikokuhyou.jpg",
-            preview_image_url=f"https://linebot-practice-12.herokuapp.com/static/jikokuhyou.jpg",
-        )
+    # if event.message.text == "時刻表":
+    #     image_message = ImageSendMessage(
+    #         original_content_url=f"https://linebot-practice-12.herokuapp.com/static/jikokuhyou.jpg",
+    #         preview_image_url=f"https://linebot-practice-12.herokuapp.com/static/jikokuhyou.jpg",
+    #     )
+    #     line_bot_api.reply_message(
+    #         event.reply_token,
+    #         image_message
+    #     )
+    # 出発時間が送られたら5件出力する
+    m = re.match(r'^([01][0-9]|2[0-3]):[0-5][0-9]$', event.message.text)
+    conn = psycopg2.connect(DATABASE_URL)
+    c = conn.cursor()
+    if m != None:
+        sql = "SELECT departure_time, arrival_time FROM jikokuhyou WHERE departure_time > '" + \
+            m.group(0)+"' limit 5;"
+        c.execute(sql)
+        ret = c.fetchall()
+
         line_bot_api.reply_message(
             event.reply_token,
-            image_message
-        )
-    else:
-        # 出発時間が送られたら5件出力する
-        m = re.match(r'^([01][0-9]|2[0-3]):[0-5][0-9]$', event.message.text)
-        conn = psycopg2.connect(DATABASE_URL)
-        c = conn.cursor()
-        if m != None:
-            sql = "SELECT departure_time, arrival_time FROM jikokuhyou WHERE departure_time > '" + \
-                m.group(0)+"' limit 5;"
-            c.execute(sql)
-            ret = c.fetchall()
+            messages=TemplateSendMessage(
+                alt_text="時刻検索結果",
+                template=ButtonsTemplate(
+                    text="バス時刻表検索",
+                    actions=[
+                        PostbackTemplateAction(
+                            label=ret[0][0].strftime(
+                                "%H:%M") + "発 " + ret[0][1].strftime("%H:%M") + "着",
+                            data="is_show=0"
+                        ),
+                        PostbackTemplateAction(
+                            label=ret[1][0].strftime(
+                                "%H:%M") + "発 " + ret[1][1].strftime("%H:%M") + "着",
+                            data="is_show=1"
+                        ),
 
-            line_bot_api.reply_message(
-                event.reply_token,
-                messages=TemplateSendMessage(
-                    alt_text="時刻検索結果",
-                    template=ButtonsTemplate(
-                        text="バス時刻表検索",
-                        actions=[
-                            PostbackTemplateAction(
-                                label=ret[0][0].strftime(
-                                    "%H:%M") + "発 " + ret[0][1].strftime("%H:%M") + "着",
-                                data="is_show=0"
-                            ),
-                            PostbackTemplateAction(
-                                label=ret[1][0].strftime(
-                                    "%H:%M") + "発 " + ret[1][1].strftime("%H:%M") + "着",
-                                data="is_show=1"
-                            ),
-
-                            PostbackTemplateAction(
-                                label=ret[2][0].strftime(
-                                    "%H:%M") + "発 " + ret[2][1].strftime("%H:%M") + "着",
-                                data="is_show=2"
-                            ),
-                            PostbackTemplateAction(
-                                label=ret[3][0].strftime(
-                                    "%H:%M") + "発 " + ret[3][1].strftime("%H:%M") + "着",
-                                data="is_show=3"
-                            )
-                        ]
-                    )
+                        PostbackTemplateAction(
+                            label=ret[2][0].strftime(
+                                "%H:%M") + "発 " + ret[2][1].strftime("%H:%M") + "着",
+                            data="is_show=2"
+                        ),
+                        PostbackTemplateAction(
+                            label=ret[3][0].strftime(
+                                "%H:%M") + "発 " + ret[3][1].strftime("%H:%M") + "着",
+                            data="is_show=3"
+                        )
+                    ]
                 )
             )
-        else:
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(
-                    text="出発したい時間を入力してください！\nその後の直近５件の時刻を教えます。\n(例)09:00, 12:00, 15:30")
-            )
+        )
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(
+                text="出発したい時間を入力してください！\nその後の直近５件の時刻を教えます。\n(例)09:00, 12:00, 15:30")
+        )
 
 
 if __name__ == "__main__":
