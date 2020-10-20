afbdee7545ad3c9b7fd4259e8936f6e9debc1d64
yukiti224gt <mamekiti084@gmail.com>
2020-10-20 15:43:10 +0900

    db追加前

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    FollowEvent, MessageEvent, TextMessage, TextSendMessage, ImageMessage, ImageSendMessage, TemplateSendMessage, ButtonsTemplate, PostbackTemplateAction, MessageTemplateAction, URITemplateAction
+    FollowEvent, MessageEvent, TextMessage, TextSendMessage, ImageMessage, ImageSendMessage, TemplateSendMessage, ButtonsTemplate, PostbackTemplateAction, MessageTemplateAction, URITemplateAction, PostbackEvent
 )
+import psycopg2
+from psycopg2.extras import DictCursor
+
 import os
+import sys
+import json
+import re
 # 軽量なウェブアプリケーションフレームワーク:Flask
 app = Flask(__name__)  # 環境変数からLINE Access Tokenを設定
 # 環境変数からLINE Channel Secretを設定
@@ -18,8 +24,10 @@ LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
+DATABASE_URL = os.environ.get('HEROKU_POSTGRESQL_ONYX_URL')
 
-@app.route("/callback", methods=['POST'])
+
+@ app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
@@ -32,9 +40,8 @@ def callback():
     except InvalidSignatureError:
         abort(400)
     return 'OK'
-# MessageEvent
 
-  try:
+    try:
         conn = psycopg2.connect(DATABASE_URL)
         c = conn.cursor()
         sql = "SELECT name FROM user_data WHERE id = '"+user_id+"';"
@@ -53,54 +60,54 @@ def callback():
         c.close()
 
 
-@handler.add(MessageEvent, message=TextMessage)
+@ handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text)
     )
 
-     m = re.match(r'^([01][0-9]|2[0-3]):[0-5][0-9]$', event.message.text)
-        conn = psycopg2.connect(DATABASE_URL)
-        c = conn.cursor()
-        if m != None:
-            sql = "SELECT daparture_time FROM  time_record WHERE daparture_time > '" + \
-                m.group(0)+"' limit 5;"
-            c.execute(sql)
-            ret = c.fetchall()
+    m = re.match(r'^([01][0-9]|2[0-3]):[0-5][0-9]$', event.message.text)
+    conn = psycopg2.connect(DATABASE_URL)
+    c = conn.cursor()
+    if m != None:
+        sql = "SELECT daparture_time FROM  time_record WHERE daparture_time > '" + \
+            m.group(0)+"' limit 5;"
+        c.execute(sql)
+        ret = c.fetchall()
 
-            line_bot_api.reply_message(
-                event.reply_token,
-                messages=TemplateSendMessage(
-                    alt_text="時刻検索結果",
-                    template=ButtonsTemplate(
-                        text="バス時刻表検索",
-                        actions=[
-                            PostbackTemplateAction(
-                                label=ret[0][0].strftime(
-                                    "%H:%M") + "発 " + ret[0][1].strftime("%H:%M") + "着",
-                                data="is_show=0"
-                            ),
-                            PostbackTemplateAction(
-                                label=ret[1][0].strftime(
-                                    "%H:%M") + "発 " + ret[1][1].strftime("%H:%M") + "着",
-                                data="is_show=1"
-                            ),
+        line_bot_api.reply_message(
+            event.reply_token,
+            messages=TemplateSendMessage(
+                alt_text="時刻検索結果",
+                template=ButtonsTemplate(
+                    text="バス時刻表検索",
+                    actions=[
+                        PostbackTemplateAction(
+                            label=ret[0][0].strftime(
+                                "%H:%M") + "発 " + ret[0][1].strftime("%H:%M") + "着",
+                            data="is_show=0"
+                        ),
+                        PostbackTemplateAction(
+                            label=ret[1][0].strftime(
+                                "%H:%M") + "発 " + ret[1][1].strftime("%H:%M") + "着",
+                            data="is_show=1"
+                        ),
 
-                            PostbackTemplateAction(
-                                label=ret[2][0].strftime(
-                                    "%H:%M") + "発 " + ret[2][1].strftime("%H:%M") + "着",
-                                data="is_show=2"
-                            ),
-                            PostbackTemplateAction(
-                                label=ret[3][0].strftime(
-                                    "%H:%M") + "発 " + ret[3][1].strftime("%H:%M") + "着",
-                                data="is_show=3"
-                            )
-                        ]
-                    )
+                        PostbackTemplateAction(
+                            label=ret[2][0].strftime(
+                                "%H:%M") + "発 " + ret[2][1].strftime("%H:%M") + "着",
+                            data="is_show=2"
+                        ),
+                        PostbackTemplateAction(
+                            label=ret[3][0].strftime(
+                                "%H:%M") + "発 " + ret[3][1].strftime("%H:%M") + "着",
+                            data="is_show=3"
+                        )
+                    ]
                 )
             )
+        )
 
 
 if __name__ == "__main__":
