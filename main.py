fb47566cc0333002a6b4ec501c595efc2234d671
yukiti224gt <mamekiti084@gmail.com>
2020-10-23 14:48:19 +0900

    インデントエラー修正

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ def handle_message(event):
                 )
             )
         )
-        else:
+    else:
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(
